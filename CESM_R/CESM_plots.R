9eeca3a4373015c4fb9ddd2fa20160555160cdb8
TracyMcBean <thkisz@yahoo.co.uk>
2019-05-22 18:33:58 +0200

    Additional runs added

--- CESM_R/CESM_plots.R
+++ CESM_R/CESM_plots.R
@@ -24,44 +24,49 @@ ndays10_notag <- subset(prof_not_df, NDays == 10)
 
 # Set isTagged to TRUE for tagged title
 # Total duration
-plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_tot, ndays10_tag$Cores,
-                  ndays10_tag$Dur_tot, TRUE, "Total Duration", TRUE, "5vs10_dur_tot_tagged")
+plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_tot, ndays10_notag$Cores,
+                  ndays10_notag$Dur_tot, isTagged=FALSE, "Total Duration", isSaved=FALSE, "5vs10_dur_tot_not")
 
 # Init duration
-plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_init, ndays10_tag$Cores,
-                  ndays10_tag$Dur_init, TRUE, "Initialization Duration",  TRUE, "5vs10_dur_init_tagged")
+plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_init, ndays10_notag$Cores,
+                  ndays10_notag$Dur_init, FALSE, "Initialization Duration", FALSE, "5vs10_dur_init_not")
 
 # Run duration
-plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_run, ndays10_tag$Cores,
-                  ndays10_tag$Dur_run, TRUE, "Run Duration",  TRUE, "5vs10_dur_run_tagged")
+plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_run, ndays10_notag$Cores,
+                  ndays10_notag$Dur_run, FALSE, "Run Duration",  FALSE, "5vs10_dur_run_not")
 
 # Fin duration
-plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_fin, ndays10_tag$Cores,
-                  ndays10_tag$Dur_fin, TRUE, "Finish Duration",  TRUE, "5vs10_dur_fin_tagged")
+plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_fin, ndays10_notag$Cores,
+                  ndays10_notag$Dur_fin, FALSE, "Finish Duration", FALSE, "5vs10_dur_fin_not")
 
-### Throughput ###--------------------------------------------------------------
+### Throughput and Cost ###--------------------------------------------------------------
 # See page 43 of CESM user guide 1.2 for explanation
-plot(ndays5_notag$Cores, ndays5_notag$Throughput)
-plot(ndays5_notag$Cores, ndays5_notag$Cost)
+plot_5vs10_flex(ndays5_tag$Cores, ndays5_tag$Throughput, ndays10_tag$Cores,
+                  ndays10_tag$Throughput, ytext="Simulated_years / day ", isTagged=TRUE, "Throughput", FALSE, "5vs10_troughput_tagged")
+# Cost
+# The cost is expected to increase if using more cores for CESM
+plot_5vs10_flex(ndays5_tag$Cores, ndays5_tag$Cost, ndays10_tag$Cores,
+                ndays10_tag$Cost, ytext="pe-hrs/simulated_years", isTagged=TRUE, "Cost", FALSE, "5vs10_cost_tagged")
+
 
 ### Plots both ####--------------------------------------------------------------
 # Total duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_tot, ndays10_notag$Cores,
                   ndays10_notag$Dur_tot, ndays5_tag$Cores, ndays5_tag$Dur_tot, ndays10_tag$Cores,
-                  ndays10_tag$Dur_tot, "Total Duration", FALSE, "5vs10_dur_tot_both")
+                  ndays10_tag$Dur_tot, "Total Duration", TRUE, "5vs10_dur_tot_both")
 
 # Init duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_init, ndays10_notag$Cores,
                ndays10_notag$Dur_init, ndays5_tag$Cores, ndays5_tag$Dur_init, ndays10_tag$Cores,
-               ndays10_tag$Dur_init, "Initialization Duration", FALSE, "5vs10_dur_init_both")
+               ndays10_tag$Dur_init, "Initialization Duration", TRUE, "5vs10_dur_init_both")
 
 # Run duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_run, ndays10_notag$Cores,
                ndays10_notag$Dur_run, ndays5_tag$Cores, ndays5_tag$Dur_run, ndays10_tag$Cores,
-               ndays10_tag$Dur_run, "Run Duration", FALSE, "5vs10_dur_run_both")
+               ndays10_tag$Dur_run, "Run Duration", TRUE, "5vs10_dur_run_both")
 
 # Fin duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_fin, ndays10_notag$Cores,
                ndays10_notag$Dur_fin, ndays5_tag$Cores, ndays5_tag$Dur_fin, ndays10_tag$Cores,
-               ndays10_tag$Dur_fin, "Finish Duration", FALSE, "5vs10_dur_fin_both")
+               ndays10_tag$Dur_fin, "Finish Duration", TRUE, "5vs10_dur_fin_both")
 
--- CESM_R/plot_functions.R
+++ CESM_R/plot_functions.R
@@ -10,7 +10,7 @@
 #' @param title Character string containing measured variable
 #' @param isSaved Logical, Shall the plot be saved or not
 #' @param filename Character string containing name of save plot
-plot_5vs10_single <- function(xarg, yarg, xarg2=xarg, yarg2=yarg, isTagged=FALSE, title, isSaved=FALSE, filename=""){
+plot_5vs10_single <- function(xarg, yarg, xarg2=xarg, yarg2=yarg, isTagged=FALSE, title=NA, isSaved=FALSE, filename=""){
   
   if (isTagged == TRUE){
     main_title = paste(title, "(tagged)")
@@ -23,9 +23,12 @@ plot_5vs10_single <- function(xarg, yarg, xarg2=xarg, yarg2=yarg, isTagged=FALSE
   }
   
   plot(xarg, yarg, pch=15, type = "p", col="darkblue", ylim=c(min(c(yarg,yarg2)), max(c(yarg,yarg2)) ),
-       main= main_title, xlab="Number of cores", ylab="Duration in seconds")
+       main= main_title, xlab="Number of cores", ylab="Duration in seconds", xaxt="n")
   points(xarg2, yarg2, pch=16, type = "p", col="red")
-  #TODO: Add legend!!!
+  legend("topright", legend = c("5 days", "10 days"), col= c("darkblue", "red"), pch = c(15, 16),
+         bty="n")
+  axis(1, at=c(28,56,84,112,140,168,196, 224))
+   
   
   if (isSaved == TRUE){
     dev.off()
@@ -53,10 +56,43 @@ plot_5vs10_all <- function(xarg, yarg, xarg2, yarg2, xarg3, yarg3, xarg4, yarg4,
   }
   
   plot(xarg, yarg, pch=2, type = "p", col="darkblue", ylim=c(min(c(yarg,yarg2, yarg3, yarg4)), max(c(yarg,yarg2, yarg3, yarg4)) ),
-       main= paste(title, "tagged vs no tags"), xlab="Number of cores", ylab="Duration in seconds")
+       main= paste(title, "tagged vs no tags"), xlab="Number of cores", ylab="Duration in seconds", xaxt="n")
   points(xarg2, yarg2, pch=2, type = "p", col="red")
   points(xarg3, yarg3, pch=16, type = "p", col="darkblue")
   points(xarg4, yarg4, pch=16, type = "p", col="red")
+  legend("topright", legend = c("5 days no tags", "10 days no tags", "5 days tagged", "10 days tagged"),
+         col= c("darkblue", "red", "darkblue", "red"), pch = c(2, 2, 16, 16), bty = "n")
+  axis(1, at=c(28,56,84,112,140,168,196, 224))
+  grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+       lwd =1, equilogs = TRUE)
+  # TODO: Add grid to make reading easier.
+  
+  if (isSaved == TRUE){
+    dev.off()
+  }
+}
+
+# Same as plot_5vs10_single but axis label is dynamic.
+#' @param ytext label of y axis
+plot_5vs10_flex <- function(xarg, yarg, xarg2=xarg, yarg2=yarg, ytext="", isTagged=FALSE, title=NA, isSaved=FALSE, filename=""){
+  
+  if (isTagged == TRUE){
+    main_title = paste(title, "(tagged)")
+  } else {
+    main_title = paste(title, "(no tags)")
+  }
+  
+  if (isSaved == TRUE){
+    png(filename = paste("/home/tracy/Documents/IASS/Profiling_CESM/CESM_R/Plots/", filename, ".png", sep = ""))
+  }
+  
+  plot(xarg, yarg, pch=15, type = "p", col="darkblue", ylim=c(min(c(yarg,yarg2)), max(c(yarg,yarg2)) ),
+       main= main_title, xlab="Number of cores", ylab=ytext, xaxt="n")
+  points(xarg2, yarg2, pch=16, type = "p", col="red")
+  legend("topright", legend = c("5 days", "10 days"), col= c("darkblue", "red"), pch = c(15, 16),
+         bty="n")
+  axis(1, at=c(28,56,84,112,140,168,196, 224))
+  
   
   if (isSaved == TRUE){
     dev.off()
