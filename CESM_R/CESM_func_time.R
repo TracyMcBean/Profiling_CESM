117d5380cb9aeb280b7991c6f0c8bc63425efc74
TracyMcBean <thkisz@yahoo.co.uk>
2019-05-23 17:35:27 +0200

    Timer plot added

--- CESM_R/CESM_func_time.R
+++ CESM_R/CESM_func_time.R
@@ -16,20 +16,20 @@ rm(timing_data)
 # Create data file
 row_names=c(NULL)
 column_names=c("ID", "tagged", "cores", "ndays", "tot_time_run", "walltotal", "wallmax", "wallmin")
-func_df <- array(NA, dim = c(40,8,length(func_names)), dimnames=list(row_names, column_names, func_names))
+func_df <- array(NA, dim = c(42,8,length(func_names)), dimnames=list(row_names, column_names, func_names))
 # number of runs added initialize
 nrun <- 1
 
 # Some runs from 1 missing. Ending at 190521-114744
 dateID <- c(190515, 190515, 190515, 190516, 190516, 190517, 190518, 190519, 190519, 190519, 190519, 190519, 190520,
-            190520, 190520, 190521, 190521, 190515, 190515, 190516, 190516, 190516,
-            190517, 190517, 190518, 190518, 190519, 190519, 190520, 190520, 190520,
-            190520, 190520, 190520, 190520, 190520)
+            190520, 190520, 190521, 190521, 190521, 190521, 190522, 190522, 190523,
+            190515, 190515, 190516, 190516, 190516, 190517, 190517, 190518, 190518, 190519, 190519, 190520, 190520, 
+            190520, 190520, 190520, 190520, 190520, 190520)
 # numbers starting with zeros lose zeros if not saved as character explicitly
 timeID <- c(100256, 112230, 160758, "095004", 123658, 113255, 115631, "094444", 103838, 111813, 204830, 214334, 113921,
-            140026, 181204, "094851", 114744, 101006, 105339, "093632", 113246, 120848,
-            112213, 115727, 155302, 164356, 202752, 225842, 100705, 105507, 112847,
-            115041, 135839, 143315, 150757, 161950)
+            140026, 181204, "094851", 114744, 134459, 144738, "083656", 174343, "091805",
+            101006, 105339, "093632", 113246, 120848, 112213, 115727, 155302, 164356, 202752, 225842, 100705, 105507,
+            112847, 115041, 135839, 143315, 150757, 161950)
 
 for (i in 1:length(dateID)){
   list_data <- add_func_data(paste("~/Documents/IASS/Profiling_CESM/Timing_data/ccsm_timing_stats.",dateID[i], "-", as.character(timeID[i]), sep=""),
@@ -52,17 +52,34 @@ for (i in 1:length(func_df[1,1,])){
   ymin <- min(wallclock_per_not, wallclock_per_tag,na.rm = TRUE)
   ymax <- max(wallclock_per_not, wallclock_per_tag,na.rm = TRUE)
   # plot
-  #png(paste("~/Documents/IASS/Profiling_CESM/CESM_R/Plots/", names(func_df[1,1,])[i], "_10days", sep=""))
+  png(paste("~/Documents/IASS/Profiling_CESM/CESM_R/Plots/", names(func_df[1,1,])[i], "_10days", sep=""))
   plot(cores_tag, wallclock_per_tag, ylim=c(ymin, ymax),
        ylab = "% of total", xlab="Cores", main = paste("Wallclock time", names(func_df[1,1,])[i], "(10 day run)"), pch=15, col="red" )
   points(cores_not, wallclock_per_not, col = "darkgreen", pch = 16)
   legend("topleft", legend=c("tagged", "no tags"), col=c("red", "darkgreen"), pch = c(15,16), bty="n")
-  #dev.off()
+  dev.off()
 }
 
-i <- 1
+wc_means <- array(NA, dim = c(length(func_names),2))
+wc_diff <- array(NA, dim = length(func_names))
 # Check out the situtation of the average values and their spread.
-wallclock_per_tag <- (func_df[which(func_df[,4,i]==10 & func_df[,2,i]==1),7,i]/ func_df[which(func_df[,4,i]==10 & func_df[,2,i]==1),5,i]) *100
-round(mean(wallclock_per_tag), digits=3)
+for (i in 1:length(func_names)){
+  wallclock_per_tag <- (func_df[which(func_df[,4,i]==10 & func_df[,2,i]==1),7,i]/ func_df[which(func_df[,4,i]==10 & func_df[,2,i]==1),5,i]) *100
+  wallclock_per_not <- (func_df[which(func_df[,4,i]==10 & func_df[,2,i]==0),7,i]/ func_df[which(func_df[,4,i]==10 & func_df[,2,i]==0),5,i]) *100
+  
+  wc_means[i,1] <- round(mean(wallclock_per_tag, na.rm = TRUE), digits=5)
+  wc_means[i,2] <- round(mean(wallclock_per_not, na.rm=TRUE), digits=5)
+  
+  wc_diff[i] <- diff(wc_means[i,])
+}
 
 
+# If the difference is positive then the non tagged version took longer for that timer.
+par(mar=c(5,9,4,2)+0.1)
+barplot(wc_diff[which(wc_diff > 3 | wc_diff < -3)], horiz=TRUE, col = c("green", rep("blue",6), rep("green", 13)),
+        main = "Timer difference", sub = "Wallclock time of timer taken in percent of total wallclock time of run", xlab = "Difference Tagged - No tag in %")
+mtext(side = 2, text = func_names[which(wc_diff > 3 | wc_diff < -3)], las=1, at=seq(0.7,23, 1.21), cex=0.8)
+abline(h=seq(0,23, 1.21), col="grey", lty="dotted")
+#TODO: How to include variance?
+
+      
--- CESM_R/CESM_plots.R
+++ CESM_R/CESM_plots.R
@@ -21,52 +21,53 @@ ndays10_notag <- subset(prof_not_df, NDays == 10)
 
 
 #### PLOTS ####-------------------------------------------------------------------------
-
+saveVar <- FALSE
 # Set isTagged to TRUE for tagged title
 # Total duration
-plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_tot, ndays10_notag$Cores,
-                  ndays10_notag$Dur_tot, isTagged=FALSE, "Total Duration", isSaved=FALSE, "5vs10_dur_tot_not")
+plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_tot, ndays10_tag$Cores,
+                  ndays10_tag$Dur_tot, isTagged=TRUE, "Total Duration", isSaved=saveVar, "5vs10_dur_tot_tag")
 
 # Init duration
-plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_init, ndays10_notag$Cores,
-                  ndays10_notag$Dur_init, FALSE, "Initialization Duration", FALSE, "5vs10_dur_init_not")
+plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_init, ndays10_tag$Cores,
+                  ndays10_tag$Dur_init,TRUE, "Initialization Duration", saveVar, "5vs10_dur_init_tag")
 
 # Run duration
-plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_run, ndays10_notag$Cores,
-                  ndays10_notag$Dur_run, FALSE, "Run Duration",  FALSE, "5vs10_dur_run_not")
+plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_run, ndays10_tag$Cores,
+                  ndays10_tag$Dur_run, TRUE, "Run Duration",  saveVar, "5vs10_dur_run_tag")
 
 # Fin duration
-plot_5vs10_single(ndays5_notag$Cores, ndays5_notag$Dur_fin, ndays10_notag$Cores,
-                  ndays10_notag$Dur_fin, FALSE, "Finish Duration", FALSE, "5vs10_dur_fin_not")
+plot_5vs10_single(ndays5_tag$Cores, ndays5_tag$Dur_fin, ndays10_tag$Cores,
+                  ndays10_tag$Dur_fin, TRUE, "Finish Duration",  saveVar, "5vs10_dur_fin_tag")
 
 ### Throughput and Cost ###--------------------------------------------------------------
 # See page 43 of CESM user guide 1.2 for explanation
 plot_5vs10_flex(ndays5_tag$Cores, ndays5_tag$Throughput, ndays10_tag$Cores,
-                  ndays10_tag$Throughput, ytext="Simulated_years / day ", isTagged=TRUE, "Throughput", FALSE, "5vs10_troughput_tagged")
+                  ndays10_tag$Throughput, ytext="Simulated_years / day ", isTagged=TRUE, "Throughput", saveVar, "5vs10_troughput_tagged")
 # Cost
 # The cost is expected to increase if using more cores for CESM
 plot_5vs10_flex(ndays5_tag$Cores, ndays5_tag$Cost, ndays10_tag$Cores,
-                ndays10_tag$Cost, ytext="pe-hrs/simulated_years", isTagged=TRUE, "Cost", FALSE, "5vs10_cost_tagged")
+                ndays10_tag$Cost, ytext="pe-hrs/simulated_years", isTagged=TRUE, "Cost",  saveVar, "5vs10_cost_tagged")
 
 
 ### Plots both ####--------------------------------------------------------------
+saveVar <- FALSE
 # Total duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_tot, ndays10_notag$Cores,
                   ndays10_notag$Dur_tot, ndays5_tag$Cores, ndays5_tag$Dur_tot, ndays10_tag$Cores,
-                  ndays10_tag$Dur_tot, "Total Duration", TRUE, "5vs10_dur_tot_both")
+                  ndays10_tag$Dur_tot, "Total Duration", saveVar, "5vs10_dur_tot_both")
 
 # Init duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_init, ndays10_notag$Cores,
                ndays10_notag$Dur_init, ndays5_tag$Cores, ndays5_tag$Dur_init, ndays10_tag$Cores,
-               ndays10_tag$Dur_init, "Initialization Duration", TRUE, "5vs10_dur_init_both")
+               ndays10_tag$Dur_init, "Initialization Duration",  saveVar, "5vs10_dur_init_both")
 
 # Run duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_run, ndays10_notag$Cores,
                ndays10_notag$Dur_run, ndays5_tag$Cores, ndays5_tag$Dur_run, ndays10_tag$Cores,
-               ndays10_tag$Dur_run, "Run Duration", TRUE, "5vs10_dur_run_both")
+               ndays10_tag$Dur_run, "Run Duration",  saveVar, "5vs10_dur_run_both")
 
 # Fin duration
 plot_5vs10_all(ndays5_notag$Cores, ndays5_notag$Dur_fin, ndays10_notag$Cores,
                ndays10_notag$Dur_fin, ndays5_tag$Cores, ndays5_tag$Dur_fin, ndays10_tag$Cores,
-               ndays10_tag$Dur_fin, "Finish Duration", TRUE, "5vs10_dur_fin_both")
+               ndays10_tag$Dur_fin, "Finish Duration",  saveVar, "5vs10_dur_fin_both")
 
